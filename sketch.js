324525e5f07d327130b00dfa550ef994b266dec1
Vehmixxx <vinncentm168@gmail.com>
2024-01-13 00:08:45 -0500

    added p5.js framework

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let x = 0; 
+let y = 0;
+let bg = 0; 
+let ellipseColor = 140;
+let ellipseSize = 50;
+
+function setup() {
+    let canvas = createCanvas(3 * windowWidth, 3 * windowHeight);
+    canvas.position(0, 0);
+    canvas.style("z-index", 1);
+}
+
+function draw() {
+    clear(); 
+    x += (mouseX - x) * 0.30; 
+    y += (mouseY - y) * 0.30; 
+    // background(bg, 0); 
+    fill(ellipseColor, 40);
+    stroke(ellipseColor);
+
+    let ball = ellipse(x, y, ellipseSize, ellipseSize, );   
+}
+
+function adjustCavnas() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
