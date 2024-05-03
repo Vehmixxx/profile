5b356942a4c6fb11dbb65e4122b89da14c579d21
Vehmixxx <vinncentm168@gmail.com>
2024-05-03 13:45:36 -0400

    updated gallery

--- cursor.js
+++ cursor.js
@@ -1,13 +1,13 @@
-let x = 0; 
-let y = 0;
+let x = -15; 
+let y = -15;
 let bg = 0; 
-let ellipseColor = 140;
-let ellipseSize = 50;
+let ellipseColor = 150;
+let ellipseSize = 30;
 
 function setup() {
     let canvas = createCanvas(3 * windowWidth, 3 * windowHeight);
     canvas.position(-15, -15);
-    canvas.style("z-index", 1);
+    canvas.style("z-index", 3);
 }
 
 function draw() {
@@ -17,7 +17,7 @@ function draw() {
     fill(ellipseColor, 40);
     stroke(ellipseColor);
 
-    let ball = ellipse(x, y, ellipseSize, ellipseSize);   
+    let ball = ellipse(x, y, ellipseSize, ellipseSize);
 }
 
 function adjustCavnas() {
