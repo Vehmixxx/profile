f9c2cd3af02fb33ca15ff083f5833d6cfbcb3a33
Vehmixxx <vinncentm168@gmail.com>
2025-02-21 01:30:58 -0500

    new stuff on music page, wip

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//modal variables
 var modal = document.getElementById("modal-for-image");
 var modalImg = document.getElementById("myImage");
 var closeBtn = document.getElementById("close");
 
+//gets all images + adds clickFunc to each
 for (var i = 1; i <= 10; i++) {
     var image = document.getElementById(`image-${i}`);
     if (image) {
@@ -9,6 +11,7 @@ for (var i = 1; i <= 10; i++) {
     }
 }
 
+//when clicked, modal does things
 function clickFunc() {
     console.log("image clicked!")
     modal.style.display = "flex";
@@ -19,6 +22,7 @@ function clickFunc() {
     modalImg.src = this.src;
 }
 
+//closes modal
 closeBtn.addEventListener("click", function() {
     modal.style.animation = "fadeout 1s ease"
     modal.style.opacity = "0";
