b087584517f4066e496e250ebaab258761112265
Vehmixxx <vinncentm168@gmail.com>
2025-06-13 14:44:04 -0400

    updated layout of music section and gallery

--- scrolling.js
+++ scrolling.js
@@ -1,10 +1,61 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const scroll = new LocomotiveScroll({
+function initializeLocoScroll() {
+  const scroll = new LocomotiveScroll({
       el: document.querySelector("[data-scroll-container]"),
       smooth: true,
-      multiplier: 1,
+      multiplier: 0.2,
+      class: 'is-reveal',
+      smartphone: {
+        smooth: true,
+        multiplier: 0.5
+      },
+      tablet: {
+        smooth: true,
+        multiplier: 0.5
+      }
+    });
+
+    window.addEventListener('resize', () => {
+    scroll.update();
+  });
+
+  return scroll;
+}
+
+function waitForImages() {
+  return new Promise((resolve) => {
+    const images = document.querySelectorAll('image');
+    let loadedImages = 0;
+    const totalImages = images.length;
+
+    if (totalImages == 0) {
+      resolve();
+      return;
+    }
+
+    images.forEach((image) => {
+      if(image.complete) {
+        loadedImages++;
+        if(loadedImages == totalImages) {
+          resolve();
+        }
+      } else {
+        img.addEventListener('load', () => {
+          loadedImages++;
+          if (loadedImages === totalImages) {
+            resolve();
+          }
+        });
+
+        img.addEventListener('error', () => {
+          loadedImages++;
+          if (loadedImages === totalImages) {
+            resolve();
+          }
+        });
+      }
     });
   });
+}
 
   const handleAnimationEnd = (element) => {
     element.addEventListener('animationend', () => {
